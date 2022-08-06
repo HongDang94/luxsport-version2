6e12d3bdb08a4a7f51ce057ef32ecacdd536c700
HongDang94 <hong.thdang@gmail.com>
2022-08-06 12:46:24 +0700

    upADTE menu

--- js/main.js
+++ js/main.js
@@ -237,29 +237,4 @@ jQuery(document).ready(function ($) {
       },
     },
   });
-
-  var openMenu = function () {
-    $(".btn_menu").on("click", function () {
-      $(".gnb").show();
-      $("body").addClass("show_menu");
-      $(".gnb")
-        .stop()
-        .animate({ left: "0" }, 600, "easeInOutExpo", function () {});
-    });
-  };
-
-  var closeMenu = function () {
-    $(".btn_close").on("click", function () {
-      $("body").removeClass("show_menu");
-      $(".gnb")
-        .stop()
-        .animate({ left: "-100%" }, 600, "easeInOutExpo", function () {
-          //scrollLock(false);
-          $(".gnb").hide();
-        });
-    });
-  };
-
-  openMenu();
-  closeMenu();
 });
